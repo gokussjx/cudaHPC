6715fec0075836b8c47d8992460ac65788a0832c
EC2 Default User <ec2-user@ip-172-31-40-139.us-west-2.compute.internal>
2016-04-27 04:14:11 +0000

    fix standard

--- src/standard.c
+++ src/standard.c
@@ -1,12 +1,20 @@
 #include "standard.h"
 int filter_size;
 color *begin, *end, tmp[3], blkcpy2d_out[300];
-#define INPUT(x,y) begin[((y)*(512*3))+(x)*3]
+#ifdef P6
+#define INPUT(x,y) begin[((y)*(DIM*3))+(x)*3]
+#endif
+
+#ifdef P5
+#define INPUT(x,y) begin[((y)*(DIM))+(x)]
+#endif
+
 #define OUTPUT(x,y) blkcpy2d_out[(y)*filter_size+(x)]
 void load(int fd){
     int ct0a=4; struct stat _fstat;
     if (fstat(fd, &_fstat) == -1) { perror("fstat()"); exit(1); }
     color *p=mmap(NULL, _fstat.st_size, PROT_READ, MAP_PRIVATE, fd, 0), *a=p;
+    if(!(a[0]=='P'&&a[1]==MagicNumber)) { fprintf(stderr, "Unsupported format\n"); exit(1); }
     do{ p=memchr(p, 0x0a, 64)+1; } while(--ct0a);
     fwrite(a,p-a,1,stdout);
     begin=p; end=a+_fstat.st_size;
@@ -18,24 +26,24 @@ int zero_mapping(int x, int y){
 
 int buffered_mapping(int x, int y){
     if(x<0)x=0;
-    else if(x>=512)x=(512-1);
+    else if(x>=DIM)x=(DIM-1);
     if(y<0)y=0;
-    else if(y>=512)y=(512-1);
+    else if(y>=DIM)y=(DIM-1);
     return INPUT(x,y);
 }
 
 int wrapped_mapping(int x, int y){
-    if(x<0)x+=512;
-    else if(x>=512)x-=512;
-    if(y<0)y+=512;
-    else if(y>=512)y-=512-1;
+    if(x<0)x+=DIM;
+    else if(x>=DIM)x-=DIM;
+    if(y<0)y+=DIM;
+    else if(y>=DIM)y-=DIM-1;
     return INPUT(x,y);
 }
 
 void blkcpy2d(int _x, int _y){
     int r=(filter_size>>1),x1=_x-r,y1=_y-r,x2=_x+r,y2=_y+r;
     for(int y=y1;y<=y2;++y)for(int x=x1;x<=x2;++x)
-        OUTPUT(x-x1,y-y1)=(x>=0&&x<512&&y>=0&&y<512)?INPUT(x,y):buffered_mapping(x,y);
+        OUTPUT(x-x1,y-y1)=(x>=0&&x<DIM&&y>=0&&y<DIM)?INPUT(x,y):buffered_mapping(x,y);
 }
 
 int ccmp(const void *a, const void *b){
@@ -53,9 +61,15 @@ int median_filter(int x, int y){
 int main(int argc, char *argv[]){
     if(argc<3){ fprintf(stderr, "Usage: filter_size input.ppm\n"); return 1; }
     filter_size=atoi(argv[1]); load(open(argv[2], O_RDONLY));
-    for(int y=0;y<512;++y)for(int x=0;x<512;++x){
+    for(int y=0;y<DIM;++y)for(int x=0;x<DIM;++x){
+#ifdef P6
         memset(tmp, median_filter(x,y), 3);
         fwrite(tmp, 3, 1, stdout);
+#endif
+
+#ifdef P5
+        fwrite(median_filter(x,y), 1, 1, stdout);
+#endif
     }
     return 0;
 }
